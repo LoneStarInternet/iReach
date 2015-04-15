cbba3817c17799afebbe1c63385f97eac4087763
Christopher Louis Hauboldt <chris@hauboldt.us>
2015-04-15 11:17:05 -0500

    default admin user

---
+++ db/migrate/20150415160728_create_admin_user.rb
@@ -0,0 +1,10 @@
+class CreateAdminUser < ActiveRecord::Migration
+  def up
+    user = User.create(
+      email: 'admin@example.com',
+      password: 'Secret5!',
+      password_confirmation: 'Secret5!'
+    )
+    user.update_attribute(:confirmed_at, Time.now.utc)
+  end
+end
