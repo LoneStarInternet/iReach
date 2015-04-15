54ee6c1927f8078ca9738c6e94930b87c83344ab
Christopher Louis Hauboldt <chris@hauboldt.us>
2015-04-15 11:21:00 -0500

    default admin user

--- db/migrate/20150415160728_create_admin_user.rb
+++ db/migrate/20150415160728_create_admin_user.rb
@@ -5,6 +5,7 @@ class CreateAdminUser < ActiveRecord::Migration
       password: 'Secret5!',
       password_confirmation: 'Secret5!'
     )
+    user.roles << :admin
     user.update_attribute(:confirmed_at, Time.now.utc)
   end
 end
