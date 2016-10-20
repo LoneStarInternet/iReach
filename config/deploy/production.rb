8664360be555beb854c8298cdad75c8a424fb8a5
Chris Hauboldt <chauboldt@lone-star.net>
2016-10-20 12:50:06 -0500

    leela => rails deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :application, "thaa.lnstar.com"
 set :deploy_to, "/var/www/#{application}"
-set :domain, "leela.lnstar.com"
+set :domain, "rails.lnstar.com"
 set :user, "deploy"
 set :web_user, 'wwwrails'
 set :rails_env, "production"
